09fae99d0c2ae29b4339fd6b7e43561d9ca52cc7
SuperVK <victor.klomp@gmail.com>
2019-06-17 10:50:52 +0200

    updated gehoorbescherming
    by lars

--- simulations/speakerbel/main.js
+++ simulations/speakerbel/main.js
@@ -2,3 +2,4 @@ const I0 = 1e-12
 const canvas1 = document.getElementById('canvas')
 const infoDOM = document.getElementById('info')
 const disco = new Disco(40, 20, canvas1, infoDOM)
+let  protection = 20;
