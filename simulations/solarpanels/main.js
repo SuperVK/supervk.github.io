e0a0d4a5f34abfc810cee41e065fdf99059895fd
victor <victor.klomp@gmail.com>
2019-06-20 21:49:12 +0200

    added solarpanel config

--- simulations/solarpanels/main.js
+++ simulations/solarpanels/main.js
@@ -2,6 +2,13 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+canvas.width = window.innerWidth;
+canvas.height = canvas.width/2;
+
+let info = document.getElementById('info') 
+info.style.top = canvas.height+10
+
+
 const SP = {
   length: 158,
   width: 80.8,
@@ -12,5 +19,8 @@ const SP = {
 }
 
 
-const scale = 0.2
+const scale = canvas.width/4000
 const rooftop = new Rooftop(4000, 2000)
+
+document.getElementById('orientation').addEventListener('change', () => rooftop.updateSettings())
+document.getElementById('angled').addEventListener('change', () => rooftop.updateSettings())
--- simulations/solarpanels/rooftop.js
+++ simulations/solarpanels/rooftop.js
@@ -4,22 +4,22 @@ class Rooftop {
     this.rooflength = length-200
     
     //1-2 (and string)
-    this.orientation = '1'
+    this.orientation = document.getElementById('orientation').value
     
     //1-2 (and string)
-    this.angled = '1'
+    this.angled = document.getElementById('angled').value
     
     
-    this.updateValues('1','1')
+    this.updateValues()
     
   }
-  updateValues(orientation, angled) {
+  updateValues() {
     this.SPwidth = 0
     this.SPlength = 0
     this.SPlengthFlat = 0
     this.SPwidthFlat = 0
     
-    if(angled == '1') {
+    if(this.angled == '2') {
       this.SPwidth = ((SP.width+10)*Math.sin(RADIANS(SP.angle)))/Math.tan(RADIANS(90-SP.angle))+((SP.width+10)*Math.cos(RADIANS(SP.angle)))
       this.SPlength = SP.length+20
       this.SPwidthFlat = SP.width*Math.cos(RADIANS(SP.angle))
@@ -33,38 +33,72 @@ class Rooftop {
     
     this.SPInLength = 0
     this.SPInWidth = 0
-    if(orientation == '1') {
+    if(this.orientation == '2') {
       this.SPInLength = Math.floor(this.rooflength/this.SPlength)
       this.SPInWidth = Math.floor(this.roofwidth/this.SPwidth)
     } else {
       this.SPInLength = Math.floor(this.rooflength/this.SPwidth)
       this.SPInWidth = Math.floor(this.roofwidth/this.SPlength)
     }
+    console.log(this.SPInLength, this.SPInWidth)
     this.totalSPs = this.SPInLength*this.SPInWidth
-    console.log(this.totalSPs)
+    
+    if(this.orientation == '2') {
+      this.spacing = {
+        x: this.SPwidth*scale,
+        y: this.SPlength*scale
+      }
+    } else {
+      this.spacing = {
+        x: this.SPlength*scale,
+        y: this.SPwidth*scale
+      }
+    }
+    this.lineWidth = {
+      x: (this.SPwidth-this.SPwidthFlat)*scale,
+      y: (this.SPlength-this.SPlengthFlat)*scale
+    }
+
+
     this.draw()
   }
   draw() {
     ctx.fillStyle = 'lightgrey'
     ctx.fillRect(0,0,canvas.width,canvas.height)
-    for(let x = 0; x < this.SPInLength; x++) {
-      for(let y = 0; y < this.SPInWidth; y++) {
+    
+    for(let x = 0; x < this.SPInWidth; x++) {
+     
+      for(let y = 0; y < this.SPInLength; y++) {
+        
         let gradient;
         if(this.angled == '2') {
-          gradient = ctx.createLinearGradient(x*this.SPwidth*scale, 0, (x+1)*this.SPwidthFlat*scale, 0)
+          gradient = ctx.createLinearGradient((this.spacing.x*x+100*scale)-this.lineWidth.x, 0, ((x+1)*this.spacing.x+100*scale)-this.lineWidth.x, 0)
         } else {
-          gradient = ctx.createLinearGradient(0, y*this.SPlength*scale, 0, (y+1)*this.SPlengthFlat*scale)
+          gradient = ctx.createLinearGradient(0, (y*this.spacing.y+100*scale-this.lineWidth.y), 0, ((y+1)*this.spacing.y+100*scale)-this.lineWidth.y)
         }
         gradient.addColorStop(0, 'white')
         gradient.addColorStop(1, 'black')
         ctx.fillStyle = gradient
-        if(this.orientation == '2') {
-          ctx.fillRect(x*this.SPwidth*scale, y*this.SPlength*scale, (x+1)*this.SPwidthFlat*scale, (y+1)*this.SPlengthFlat*scale)
-        } else {
-          ctx.fillRect(x*this.SPwidth*scale, y*this.SPlength*scale, (x+1)*this.SPwidthFlat*scale, (y+1)*this.SPlengthFlat*scale)
-        }
+   
+        ctx.fillRect((x*this.spacing.x)+100*scale, (y*this.spacing.y)+100*scale, this.spacing.x, this.spacing.y)
+        ctx.beginPath()
+        ctx.fillStyle = 'lightgrey'
+        ctx.rect(100*scale, (this.spacing.y*(y+1)-this.lineWidth.y)+100*scale, (this.spacing.x*this.SPInWidth),this.lineWidth.y)
+        ctx.fill()
+        ctx.beginPath()
       }
+      ctx.beginPath()
+      ctx.fillStyle = 'lightgrey'
+      ctx.rect((this.spacing.x*(x+1)-this.lineWidth.x)+100*scale, 100*scale, this.lineWidth.x, (this.spacing.y*this.SPInLength))
+      ctx.fill()
+      
     }
+
+  }
+  updateSettings() {
+    this.orientation = document.getElementById('orientation').value
+    this.angled = document.getElementById('angled').value
+    this.updateValues()
   }
 }
 let RADIANS = (degrees) => degrees * (Math.PI/180);
