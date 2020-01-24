ca4fda95e93e105babee16c647d36c07dc6b4347
SuperVK <victor.klomp@gmail.com>
2020-01-24 23:50:49 +0100

    added huetify

---
+++ huetify/precache-manifest.bd03a5ffd70b9cfc14e22aaaedf6c0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda5eebd63af2039da3665d2584bba64",
+    "url": "/huetify/index.html"
+  },
+  {
+    "revision": "62070d84ade7dea316f2",
+    "url": "/huetify/static/css/main.12fa6574.chunk.css"
+  },
+  {
+    "revision": "4daf2c6bdffe82a39e6e",
+    "url": "/huetify/static/js/2.f25c8919.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/huetify/static/js/2.f25c8919.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62070d84ade7dea316f2",
+    "url": "/huetify/static/js/main.a7b552a4.chunk.js"
+  },
+  {
+    "revision": "69065685120d7fb2ce6b",
+    "url": "/huetify/static/js/runtime-main.8ba47350.js"
+  },
+  {
+    "revision": "56ef97592f591a714dbe38a2539cbe1f",
+    "url": "/huetify/static/media/corner.56ef9759.svg"
+  }
+]);
