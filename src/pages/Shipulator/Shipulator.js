740c8d42ea638987d789499018d1b7c7e1e4e59f
SuperVK <victor.klomp@gmail.com>
2020-04-22 18:29:36 +0200

    added the shipulator page

--- src/pages/Shipulator/Shipulator.js
+++ src/pages/Shipulator/Shipulator.js
@@ -5,7 +5,11 @@ export default class Shipulator extends Component {
     render() {
         return (
             <div className="ShipulatorApp">
-                <div className="wip">WORK IN POGRESS</div>
+                <div className="ShipTitle">Shipulator</div>
+                <div className="ShipDesc">
+                    Shipulator is an abstract manager game with inspiration taken from clicker games. I'm still working on it.
+                </div>
+                <div className="ShipDownloadButton">SOON</div>
             </div>
         )
     }
