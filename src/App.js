7633e76bfb79651eda417fb5a630754385d3b528
SuperVK <victor.klomp@gmail.com>
2020-04-24 00:11:43 +0200

    fixed some routing issues

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     componentDidMount() {
         let fakeUrl = new URL(window.location)
         if(!fakeUrl.searchParams.has('q')) return
-        let actualURL = new URL(fakeUrl.searchParams.get('q'), 'http://' + window.location.host)
+        let actualURL = new URL(fakeUrl.searchParams.get('p'), 'http://' + window.location.host)
         let state = actualURL.pathname.split('/')[1].toUpperCase()
         if(state === 'HOME') {
             window.history.replaceState(null, '', '/')
